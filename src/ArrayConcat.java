8a965f213418ddc9c8d1b2301aa36a4144f55e4e
priyanka Biradar <pbiradar397@gmail.com>
2022-06-10 21:41:48 +0530

    committing concatenation of two arrays

--- src/ArrayConcat.java
+++ src/ArrayConcat.java
@@ -1,21 +1,32 @@
 public class ArrayConcat {
     public static void main(String[] args) {
         int[] a = {4, 5, 6};
+        int[] b = {7, 8, 9, 10};
 
-        int n = a.length;
+
+//        int n = a.length;
 
 
-        int[] c = new int[2 * n];
-        concatArray(a, n, c);
+        int[] c = new int[a.length + b.length];
+        concatArray(a,b,c);
     }
 
-    private static void concatArray(int[] a, int n, int[] c) {
-        for (int i = 0; i < 2 * n; i++) {
-
-            c[i] = a[i % n];
-            System.out.print(c[i] + " ");
-
+    private static void concatArray(int[] a, int[] b, int[] c) {
+        for (int i = 0; i < a.length; i++) {
+           c[i] = a[i];
+//           i=0 c=4,i=1 c[1]=5
+        }
+        for (int j=a.length,i=0;j<c.length;j++,i++) {
+            c[j]=b[i];
 
         }
+        for (int i = 0; i < c.length ; i++) {
+            System.out.print(c[i] + " ");
+
+        }
+
+
+
+
     }
 }
