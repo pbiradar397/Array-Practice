9c6f9fbd832a9fd8a47dc4abac34700d4b40a48e
priyanka Biradar <pbiradar397@gmail.com>
2022-06-29 19:51:50 +0530

    committing second method using swap of reversing array

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -1,23 +1,21 @@
 public class ReverseArray {
     static void reverse(int[] a, int n) {
-
-        int[] b = new int[n];
-        int j = n;
-
-        for (int i = 0; i < n; i++) {
-            b[j - 1] = a[i];
-            j = j - 1;
-
-        }
-        System.out.println("Given array is ");
+        System.out.println("Given array is: ");
         for (int i = 0; i < n; i++) {
             System.out.print(a[i] + " ");
         }
+        int i, k, temp;
+        for (i = 0; i < n / 2; i++) {
+            temp = a[i];
+            a[i] = a[n - i - 1];
+            a[n - i - 1] = temp;
+        }
+
         System.out.println();
 
         System.out.println("reversed array is:");
-        for (int k = 0; k < n; k++) {
-            System.out.print(b[k] + " ");
+        for (k = 0; k < n; k++) {
+            System.out.print(a[k] + " ");
         }
 
     }
